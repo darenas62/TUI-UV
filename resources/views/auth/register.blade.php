4fb029b32868fe75e8c47a68a25d084cd30ca62f
U-MIGUEL-PC\Miguel <mgonzalez@produccion.datactil.com>
2015-09-11 06:42:55 -0300

    Fix 'registro' title

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
-                <div class="panel-heading">Rwgistrar</div>
+                <div class="panel-heading">Registrar</div>
  
                 <div class="panel-body">
                     {!! Form::open(['route' => 'auth/register', 'class' => 'form']) !!}
