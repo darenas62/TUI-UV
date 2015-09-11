c915982efffe3d66c64c404acabcfd78fa66210e
darenas62 <darenas62@gmail.com>
2015-09-11 01:28:48 -0300

    Vistas
    Usuario,Secretaria,Admin. Se ven xD

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,3 +29,11 @@ Route::get('auth/logout', ['as' => 'auth/logout', 'uses' => 'Auth\AuthController
 // Registration routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', ['as' => 'auth/register', 'uses' => 'Auth\AuthController@postRegister']);
+
+// Usuarios
+Route::get('secretaria', [
+	'as' => 'secretaria',
+	'uses' => 'Users\secretariaController@index'
+	]);
+Route::get('estudiante', 'Users\estudianteController@index');
+Route::get('admin', 'Users\adminController@index');
