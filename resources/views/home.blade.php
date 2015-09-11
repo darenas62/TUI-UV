c16ad50f3676fd61b796d53b43478a48cdc50478
U-MIGUEL-PC\Miguel <mgonzalez@produccion.datactil.com>
2015-09-11 06:45:01 -0300

    Agregar vistas distintas por tipo de usuario

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,42 @@
           <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec rutrum congue leo eget malesuada.</p>
         </div>
       </div>
+    @if (!Auth::guest())
+      @if (Auth::user()->group == 1)
+      <!-- Estudiante -->
+      <div class="panel panel-primary">
+        <div class="panel-heading">Panel Administrador</div>
+
+        <div class="panel-body">
+          <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.</p>
+          <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec rutrum congue leo eget malesuada.</p>
+        </div>
+      </div>
+      @endif
+      @if (Auth::user()->group == 4)
+      <!-- Estudiante -->
+      <div class="panel panel-primary">
+        <div class="panel-heading">Noticias académicas</div>
+
+        <div class="panel-body">
+          <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.</p>
+          <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec rutrum congue leo eget malesuada.</p>
+        </div>
+      </div>
+      @endif
+      @if (Auth::user()->group == 3)
+      <!-- Secretaria -->
+      <div class="panel panel-primary">
+        <div class="panel-heading">Panel Secretaria</div>
+
+        <div class="panel-body">
+          <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.</p>
+          <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec rutrum congue leo eget malesuada.</p>
+        </div>
+      </div>
+      @endif
+    @endif
+
     </div>
   </div>
 </div>
